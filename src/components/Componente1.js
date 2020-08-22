d7702ee8bf2e818e628406ab4feb1e9622015128
(Benja Kunkel kunkelbenjamin08@gmail.com) <kunkelbenjamin08@gmail.com>
2020-08-22 18:28:24 -0300

    Cree mis dos primeros componentes en react :)

---
+++ src/components/Componente1.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Componente1 = () =>  ( <h1> Soy el componente1 </h1> );
+
+export default Componente1;
---
+++ src/components/PrimerComponente.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class PrimerComponente extends Component {
+  render() {
+    return (<h1> Soy PrimerComponente con class </h1>);
+  }
+}
+
+export default PrimerComponente;
